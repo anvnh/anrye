882de8fb1bf2065a38efc1582b416a7822832220
anvnh <anvo20052@gmail.com>
2025-07-28 23:53:23 +0700

    Trigger build

--- app/shared/[id]/page.tsx
+++ app/shared/[id]/page.tsx
@@ -273,7 +273,6 @@ export default function SharedNotePage() {
                     >
                       Edit
                     </button>
-
                   )}
                 </>
               )}
